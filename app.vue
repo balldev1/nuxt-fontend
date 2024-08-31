06e9b0f2c419e8de2f9d32abf873f07001fa6470
balldev1 <balldev1@hotmail.com>
2024-08-31 20:45:34 +0700

    first commit

---
+++ app.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="  p-2 h-screen bg-blue-600 ">
+    <div class=" h-full grid grid-cols-2 " >
+      <!--  t  -->
+      <div class="border-l-4 bg-blue-600 border-t-4 border-black      ">
+        <div class=" flex items-center justify-center   h-full " >
+          <h1 class="text-9xl">T</h1>
+        </div>
+      </div>
+      <!--  e  -->
+      <div class=" border-t-4 border-black  " >
+        <div class="bg-white flex items-center  justify-center  text-9xl  border-r-4 border-blue-600 h-full"  style="box-shadow: inset 0 -4px 4px -2px rgba(0, 0, 0, 0.5);">
+          <h1 class="text-9xl text-gray-700" >E</h1>
+        </div>
+      </div>
+      <!--  s  -->
+      <div class="border-b-4 border-black" style="box-shadow: inset 0 4px 4px -2px rgba(0, 0, 0, 0.5);" >
+        <div class="flex bg-white items-center  justify-center text-9xl border-l-4 border-blue-600 h-full"  >
+          <h1 class="text-9xl text-gray-700"  >S</h1>
+        </div>
+      </div>
+      <!--  t  -->
+      <div class=" border-b-4  border-black  ">
+        <div class="bg-blue-600 flex items-center justify-center   text-9xl border-black border-r-4 h-full">
+          <h1 class="text-9xl">T</h1>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</template>
+<script setup lang="ts">
+</script>
